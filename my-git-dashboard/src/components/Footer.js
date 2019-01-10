b496a80f7837bbc6f7469db0dee2968ea1507b75
Iara Ribeiro <iara.prbr@gmail.com>
2019-01-09 20:52:33 -0500

    basic template with nav and footer

---
+++ my-git-dashboard/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import githubIcon from '../img/github.svg';
+import linkedinIcon from '../img/linkedin.svg';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="Footer">
+        <div className="Social-icons">
+          <a href="https://github.com/Iararibeiro"><img src={githubIcon} className="icon" alt="Github logo" /></a>
+          <a href="https://www.linkedin.com/in/iararibeiro/"><img src={linkedinIcon} className="icon" alt="LinkedIn logo" /></a>
+        </div>
+        <p className="Signature">Iara Ribeiro 2018</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
