0e307859e8439bbeab2c8d684fc2abec248c9930
Iara Ribeiro <iara.prbr@gmail.com>
2019-01-10 20:37:58 -0500

    Including routing

---
+++ my-git-dashboard/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class About extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1> This is the amazing about i'm working on </h1>
+      </div>
+    )
+  }
+}
+
+export default About;
--- my-git-dashboard/src/components/Header.js
+++ my-git-dashboard/src/components/Header.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 import icon from '../img/icon.svg';
 
 class Header extends React.Component {
   render() {
     return (
-      <div className="App-header">
-        <ul>
-          <li className="App-name"><img src={icon} /> My Git Dashboard</li>
-          <li className="About"><a>About</a></li>
-        </ul>
-      </div>
+      <Router>
+        <div>
+        <nav className="App-header">
+          <ul className="Header-links">
+            <li className="App-name">
+              <Link to={'/'} className="Nav-link" activeClassName="active">
+                <img src={icon} /> My Git Dashboard
+              </Link>
+            </li>
+            <li className="About">
+              <Link to={'/about'} className="Nav-link" activeClassName="active">
+                About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+        </div>
+      </Router>
     );
   }
 }
