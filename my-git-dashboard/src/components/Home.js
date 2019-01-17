5c996aa3b112cc7b15a21e9890a947a5f2e9230d
Iara Ribeiro <iara.prbr@gmail.com>
2019-01-16 21:35:08 -0500

    Small silly changes

--- my-git-dashboard/src/components/Home.js
+++ my-git-dashboard/src/components/Home.js
@@ -7,7 +7,9 @@ class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userhandle: ''
+      userhandle: '',
+      userimg: octProfile,
+      hideInput: true
     };
 
     this.updateInputValue = this.updateInputValue.bind(this);
@@ -20,7 +22,10 @@ class Home extends React.Component {
 
   handleSubmit(event) {
     axios.get('https://my-git-dashboard-server.herokuapp.com/userimg/' + this.state.userhandle )
-      .then(response => console.log(response));
+      .then(response => this.setState({
+        userimg: response.data ,
+        hideInput: false
+      } ));
     event.preventDefault();
 
   }
@@ -29,13 +34,15 @@ class Home extends React.Component {
     return (
       <div className="dashboard">
         <div className="UserPicture">
-          <img src={octProfile} alt="the octcat image"/>
+          <img src={this.state.userimg} alt="the octcat image"/>
         </div>
         <div className="UserSubmit">
           <form>
             <label>
-              Type the User handle and click in generate <br />
-              <input type="text" name="name" placeholder="@userhandle" value={this.state.userhandle} onChange={this.updateInputValue}/>
+              { this.state.hideInput ? <p>Type the User handle and click in generate <br /></p> :
+                <p> This is your user?<br /></p>}
+              { this.state.hideInput ? <input type="text" name="name" placeholder="@userhandle"
+              value={this.state.userhandle} onChange={this.updateInputValue}/> : null }
               <br />
             </label>
             <button className="GenerateBtn" type="button" onClick={this.handleSubmit} >
