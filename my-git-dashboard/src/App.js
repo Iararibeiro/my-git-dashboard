c8424bd54ade9c76ebf3b113fb279043873f08c0
Iara Ribeiro <iara.prbr@gmail.com>
2019-02-20 17:27:55 -0500

    including auth

--- my-git-dashboard/src/App.js
+++ my-git-dashboard/src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import auth0Client from './utils/Auth';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Login />
+      { !auth0Client.isAuthenticated() && <Login /> }
+      { auth0Client.isAuthenticated() && <Dashboard /> }
       <Footer />
       </div>
     );
