f0c9e99167bd3735f99980713e8ae0470d688be0
Iara Ribeiro <iara.prbr@gmail.com>
2020-11-16 21:57:47 -0500

    adding build for test

---
+++ my-git-dashboard/build/precache-manifest.4f2ff9ab4f5ff7e1a9c19257010a3efc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "88428d073904e3cd8236",
+    "url": "/my-git-dashboard/static/css/main.4bfad41e.chunk.css"
+  },
+  {
+    "revision": "88428d073904e3cd8236",
+    "url": "/my-git-dashboard/static/js/main.88428d07.chunk.js"
+  },
+  {
+    "revision": "cca523cca18adc064d26",
+    "url": "/my-git-dashboard/static/js/runtime~main.cca523cc.js"
+  },
+  {
+    "revision": "c3334c8233b5c8df6084",
+    "url": "/my-git-dashboard/static/js/2.c3334c82.chunk.js"
+  },
+  {
+    "revision": "65321b98ef26842bddf4c8710527234a",
+    "url": "/my-git-dashboard/static/media/icon.65321b98.svg"
+  },
+  {
+    "revision": "246b370050d30e2417ef2f87a7a1f9d7",
+    "url": "/my-git-dashboard/static/media/code-blue.246b3700.svg"
+  },
+  {
+    "revision": "b2769f7b027f53f8f301f72ce6d5cae3",
+    "url": "/my-git-dashboard/static/media/github.b2769f7b.svg"
+  },
+  {
+    "revision": "f92c23185c51042e84b4243b65456530",
+    "url": "/my-git-dashboard/static/media/linkedin.f92c2318.svg"
+  },
+  {
+    "revision": "9b83a47488f8ebcb7db6fb9087f44ba1",
+    "url": "/my-git-dashboard/static/media/icon-pink.9b83a474.svg"
+  },
+  {
+    "revision": "4b5a7809243c8aa30b1ea29e27a74d62",
+    "url": "/my-git-dashboard/static/media/history.4b5a7809.svg"
+  },
+  {
+    "revision": "d6dd9e8ba4f1e6f89eff9823d97d284c",
+    "url": "/my-git-dashboard/static/media/open-source.d6dd9e8b.svg"
+  },
+  {
+    "revision": "1740cc025eae5ab7838d58ebbd53119c",
+    "url": "/my-git-dashboard/static/media/statistics.1740cc02.svg"
+  },
+  {
+    "revision": "e8fd65cfdfb04a043f25cbe94259061b",
+    "url": "/my-git-dashboard/static/media/number-1.e8fd65cf.svg"
+  },
+  {
+    "revision": "c51dfff13f297dc886b8da745df8a929",
+    "url": "/my-git-dashboard/static/media/number-2.c51dfff1.svg"
+  },
+  {
+    "revision": "8aa3041e726ef1890fb85b8d50e4c96f",
+    "url": "/my-git-dashboard/static/media/number-3.8aa3041e.svg"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/my-git-dashboard/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "0e89061971a41d02dd80eaba84efe930",
+    "url": "/my-git-dashboard/index.html"
+  }
+];
